d9878f8848ac25dd603f0e8126bd8e3f1ea40cbc
unknown <jgonzalez-i121nn@kinal.edu.gt>
2025-12-11 01:33:27 -0600

    Agregar funcionalidad del Login

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,41 @@ import './App.css'
 import { AppBarMenu } from './components/layout/AppBarMenu'
 import { useState } from 'react'
 import { SideNav } from './components/layout/SideNav';
+import { LoginForm } from './components/auth/LoginForm';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 function App() {
 
-  const[draweOpen, setDrawerOpen] = useState(false);
+  const [draweOpen, setDrawerOpen] = useState(false);
   const handleDrawerToggle = () => setDrawerOpen(!draweOpen);
+
   const handleLogout = () => {
-    console.log('Sesion cerrada')
+    Swal.fire({
+      title: 'Logout',
+      text: 'Estas seguro de cerrar Sesión?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.location.href = '/login';
+      }
+    });
+
   }
 
   return (
-    <>
-     <CssBaseline/>
-     <AppBarMenu onMenuClick={handleDrawerToggle} onLogout={handleLogout}></AppBarMenu>
-     <SideNav open={draweOpen} onClose={handleDrawerToggle}></SideNav>
-    </>
+    <Router>
+      <CssBaseline />
+      <AppBarMenu onMenuClick={handleDrawerToggle} onLogout={handleLogout}></AppBarMenu>
+      <SideNav open={draweOpen} onClose={handleDrawerToggle}></SideNav>
+      <Routes>
+        <Route path='/login' element={<LoginForm onLoginSuccess={() => window.location.href = 'www.google.com.gt'} />} />
+      </Routes>
+    </Router>
   )
 }
 
